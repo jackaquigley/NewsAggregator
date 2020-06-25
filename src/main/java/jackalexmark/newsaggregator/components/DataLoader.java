582ed3baae41d8cb736466718f06d24fc74ff920
Mark Nicol <marknicol1991@live.co.uk>
2020-06-25 02:16:45 +0100

    Adds more seed data.

--- src/main/java/jackalexmark/newsaggregator/components/DataLoader.java
+++ src/main/java/jackalexmark/newsaggregator/components/DataLoader.java
@@ -175,5 +175,95 @@ public class DataLoader implements ApplicationRunner {
                 story5, 121, independent);
         sourceRepository.save(story5Source3);
 
+        Story story6 = new Story("Housing secretary in planning row");
+        storyRepository.save(story6);
+
+        Source story6Source1 = new Source("Westferry planning row: Jenrick texted property developer, documents show",
+                "https://ichef.bbci.co.uk/news/660/cpsprodpb/B91C/production/_113088374_mediaitem113059409.jpg",
+                "https://www.bbc.co.uk/news/uk-politics-53172995",
+                story6, 45, bbc);
+        sourceRepository.save(story6Source1);
+
+        Source story6Source2 = new Source("Robert Jenrick under pressure to resign after donor-row documents released",
+                "https://i.guim.co.uk/img/media/9feb546eb7a8dea67b8458be00fcdc7ee62c0627/0_111_3500_2100/master/3500.jpg?width=605&quality=45&auto=format&fit=max&dpr=2&s=12c78fdab3f57524584c343d33d4a986",
+                "https://www.theguardian.com/politics/2020/jun/24/robert-jenrick-to-release-all-relevant-information-in-planning-row",
+                story6, 67, guardian);
+        sourceRepository.save(story6Source2);
+
+        Source story6Source3 = new Source("Robert Jenrick ‘insistent’ £1bn property development decision rushed through before Tory donor forced to pay extra taxes",
+                "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/06/15/16/robert-jenrick.jpg?w230",
+                "https://www.independent.co.uk/news/uk/politics/robert-jenrick-1bn-property-development-conservatives-donor-tax-a9584106.html",
+                story6, 55, independent);
+        sourceRepository.save(story6Source3);
+
+        Story story7 = new Story("PC murder accused shows no remorse");
+        storyRepository.save(story7);
+
+        Source story7Source1 = new Source("PC Andrew Harper murder trial: Accused teen claimed he watched Fast & Furious on night of car killing",
+                "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/06/24/16/pri155442723-1.jpg?w968",
+                "https://www.independent.co.uk/news/uk/crime/pc-andrew-harper-murder-trial-accused-teen-watched-fast-and-furious-a9583626.html",
+                story7, 75, independent);
+        sourceRepository.save(story7Source1);
+
+        Source story7Source2 = new Source("Teen accused of PC Andrew Harper murder told police he 'didn't give a f***', court told",
+                "https://i2-prod.mirror.co.uk/incoming/article22246382.ece/ALTERNATES/s810/0_JS214050461.jpg",
+                "https://www.mirror.co.uk/news/uk-news/teen-accused-pc-andrew-harper-22247350",
+                story7, 99, mirror);
+        sourceRepository.save(story7Source2);
+
+        Source story7Source3 = new Source("PC Harper murder trial: Defendants 'intent on escape'",
+                "https://ichef.bbci.co.uk/news/660/cpsprodpb/50B3/production/_111395602_01cab399-10f2-4a93-9421-4833679898e1.jpg",
+                "https://www.bbc.co.uk/news/uk-england-berkshire-53164646",
+                story7, 78, bbc);
+        sourceRepository.save(story7Source3);
+
+        Story story8 = new Story("Coronavirus: pubs, bars, cafes and restaurants to reopen in England");
+        storyRepository.save(story8);
+
+        Source story8Source1 = new Source("Coronavirus: When will pubs, bars, cafes and restaurants reopen?",
+                "https://ichef.bbci.co.uk/news/660/cpsprodpb/B66D/production/_112810764_gettyimages-1215199162.jpg",
+                "https://www.bbc.co.uk/news/business-52977388",
+                story8, 280, bbc);
+        sourceRepository.save(story8Source1);
+
+        Source story8Source2 = new Source("Coronavirus: Pubs and bars to offer al fresco dining and takeaway pints under new legislation",
+                "https://i2-prod.mirror.co.uk/incoming/article22222903.ece/ALTERNATES/s810/1_JS213248861.jpg",
+                "https://www.independent.co.uk/news/uk/politics/pubs-bars-uk-4-july-al-fresco-restaurant-takeaway-pints-coronavirus-a9584031.html",
+                story8, 200, independent);
+        sourceRepository.save(story8Source2);
+
+        Source story8Source3 = new Source("Pubs could serve people booze in the street under new laws",
+                "https://i2-prod.mirror.co.uk/incoming/article22222903.ece/ALTERNATES/s810/1_JS213248861.jpg",
+                "https://www.mirror.co.uk/news/politics/pubs-could-serve-people-booze-22247407",
+                story8, 187, bbc);
+        sourceRepository.save(story8Source3);
+
+        Story story9 = new Story("Football: Liverpool move within 2 points of league win");
+        storyRepository.save(story9);
+
+        Source story9Source1 = new Source("Liverpool move within 2 points of league win",
+                "https://ichef.bbci.co.uk/onesport/cps/800/cpsprodpb/63F4/production/_113088552_gettyimages-1251951605.jpg",
+                "https://www.bbc.co.uk/sport/football/51884264",
+                story9, 120, bbc);
+        sourceRepository.save(story9Source1);
+
+        Source story9Source2 = new Source("Liverpool cruise past Crystal Palace to put Premier League title in sight",
+                "https://i.guim.co.uk/img/media/d0c9e663316e6c9bc03c90dcd9372b836a73e55f/0_250_4560_2736/master/4560.jpg?width=620&quality=85&auto=format&fit=max&s=ec45992aa6c7a786065f7af6bdf1e3b7",
+                "https://www.theguardian.com/football/2020/jun/24/liverpool-cruise-past-crystal-palace-to-put-premier-league-title-in-sight",
+                story9, 110, guardian);
+        sourceRepository.save(story9Source2);
+
+        Source story9Source3 = new Source("Liverpool refind their swagger to move within one win of Premier League title",
+                "https://static.independent.co.uk/s3fs-public/thumbnails/image/2020/06/24/21/pri155646470.jpg?w660",
+                "https://www.independent.co.uk/sport/football/premier-league/liverpool-vs-crystal-palace-result-final-score-goals-salah-fabinho-mane-a9584301.html",
+                story9, 99, independent);
+        sourceRepository.save(story9Source3);
+
+        Source story9Source4 = new Source("Liverpool show they are not only deserved champions - but one of the best sides in Reds history",
+                "https://i2-prod.mirror.co.uk/incoming/article22248314.ece/ALTERNATES/s615b/0_Liverpool-v-Crystal-Palace-Premier-League-football-match-Anfield-Liverpool-UK-24-Jun-2020.jpg",
+                "https://www.mirror.co.uk/sport/football/news/liverpool-show-not-only-deserved-22248310",
+                story9, 105, mirror);
+        sourceRepository.save(story9Source4);
+
     }
 }
